d015fd77d1cac2c8e2967a16fd292d32eda8b7c5
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-10-14 11:35:14 +0200

    Deployment config update

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,7 @@
+console.log(process.env.NODE_ENV)
+const API_BASE = process.env.NODE_ENV === "production"
+    ? "https://anyfin-currency-exchange-api.now.sh"
+    : "http://localhost:3000"
 export default {
-    API_URL: 'http://localhost:3000/graphql'
+    API_URL: `${API_BASE}/graphql`
 }
